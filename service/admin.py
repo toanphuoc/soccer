3e6cb6399cb55a46313336b205f8c6a08553fad6
toanphuoc <patotoankhtn@yahoo.com>
2016-04-18 14:37:14 +0700

    z

--- service/admin.py
+++ service/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Country, Club, Position, Player
+from .models import Country, Club, Position, Player, Tournaments, Match
 # Register your models here.
 
 admin.site.register(Country)
 admin.site.register(Club)
-admin.site.register(Position)
-admin.site.register(Player)
+admin.site.register(Tournaments)
+admin.site.register(Match)
 
--- service/models.py
+++ service/models.py
@@ -63,6 +63,12 @@ class Tournaments(models.Model):
 	country = models.ForeignKey(Country, null=True)
 	logo = models.TextField(null=True)
 
+	class Meta:
+		ordering = ['name']
+
+	def __str__(self):
+		return self.name
+
 class Match(models.Model):
 	host_club = models.ForeignKey(Club, on_delete=models.CASCADE, null=False, related_name='host_club')
 	guest_club = models.ForeignKey(Club, on_delete=models.CASCADE, null=False, related_name='guest_club')
@@ -70,6 +76,9 @@ class Match(models.Model):
 	date = models.DateTimeField()
 	tournaments = models.ForeignKey(Tournaments, null=True, related_name='tournaments')
 
+
+	def __str__(self):
+		return '%s - %s' % (self.host_club, self.guest_club)
 	class Meta:
 		ordering = ['-date']
 
